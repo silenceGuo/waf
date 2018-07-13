6068be77fd3499e4918b638fd1fe24c6efad9114
silenceGuo <516605659@qq.com>
2018-07-13 11:22:15 +0800

    Update access.lua
    对白名单url 不做cc限制，不然白名单url 设置没有意义

--- waf/access.lua
+++ waf/access.lua
@@ -4,9 +4,11 @@ function waf_main()
     if white_ip_check() then
     elseif black_ip_check() then
     elseif user_agent_attack_check() then
+        -- 对白名单url 不做cc限制
+    elseif white_url_check() then
     elseif cc_attack_check() then
     elseif cookie_attack_check() then
-    elseif white_url_check() then
+    --elseif white_url_check() then
     elseif url_attack_check() then
     elseif url_args_attack_check() then
     --elseif post_attack_check() then
